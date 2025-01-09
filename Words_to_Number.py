580e3509381476f0e399b692e916528bc4644781
Nityant17 <nityant17@gmail.com>
2025-01-09 20:19:22 +0530

    Update Words_to_Number.py
    started working on the code, till now have just written way to take input and convert to processable format

--- Words_to_Number.py
+++ Words_to_Number.py
@@ -1,1 +1,8 @@
+c = input("Enter 'ind' for Indian format and 'west' for western format:")
+x = input("Enter Number in words:")
+l = x.split(" ")
 
+if c == "west":
+    print(1)
+elif c == "ind":
+    print(2)
