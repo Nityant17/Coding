ae78686f982b8493554d736f7ed5c67e2e1b2999
Nityant17 <nityant17@gmail.com>
2025-01-09 20:31:42 +0530

    Update Words_to_Number.py
    works for basic 100, have a basic idea of what to do just need to implent it perfectly

--- Words_to_Number.py
+++ Words_to_Number.py
@@ -1,8 +1,11 @@
 c = input("Enter 'ind' for Indian format and 'west' for western format:")
 x = input("Enter Number in words:")
 l = x.split(" ")
-
+lf = list(map(lambda x: 1 if x == "one" else x,l))
+for i in range(0,len(lf)):
+    if lf[i] == 'hundred':
+        print(lf[i-1]*100)
 if c == "west":
-    print(1)
+    print(lf)
 elif c == "ind":
-    print(2)
+    print(lf)
