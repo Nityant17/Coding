a28fc40ccc992494e135143f7d80522eedeb587f
Nityant17 <nityant17@gmail.com>
2025-01-10 20:53:54 +0530

    Update Words_to_Number.py
    made the comma system work for western format, changed the way processing lists are made, works for basic case of million need to add other cases

--- Words_to_Number.py
+++ Words_to_Number.py
@@ -6,60 +6,88 @@ lf = l.copy()
 for i in range(0,len(lf)):
     if lf[i] == "zero":
         lf[i] = 0
+        l[i] = 0
     elif lf[i] == "one":
         lf[i] = 1
+        l[i] = 1
     elif lf[i] == "two":
         lf[i] = 2
+        l[i] = 2
     elif lf[i] == "three":
         lf[i] = 3
+        l[i] = 3
     elif lf[i] == "four":
         lf[i] = 4
+        l[i] = 4
     elif lf[i] == "five":
         lf[i] = 5
+        l[i] = 5
     elif lf[i] == "six":
         lf[i] = 6
+        l[i] = 6
     elif lf[i] == "seven":
         lf[i] = 7
+        l[i] = 7
     elif lf[i] == "eight":
         lf[i] = 8
+        l[i] = 8
     elif lf[i] == "nine":
         lf[i] = 9
+        l[i] = 9
     elif lf[i] == "ten":
         lf[i] = 10
+        l[i] = 10
     elif lf[i] == "eleven":
         lf[i] = 11
+        l[i] = 11
     elif lf[i] == "twelve":
         lf[i] = 12
+        l[i] = 12
     elif lf[i] == "thirteen":
         lf[i] = 13
+        l[i] = 13
     elif lf[i] == "fourteen":
         lf[i] = 14
+        l[i] = 14
     elif lf[i] == "fifteen":
         lf[i] = 15
+        l[i] = 15
     elif lf[i] == "sixteen":
         lf[i] = 16
+        l[i] = 16
     elif lf[i] == "seventeen":
         lf[i] = 17
+        l[i] = 17
     elif lf[i] == "eighteen":
         lf[i] = 18
+        l[i] = 18
     elif lf[i] == "nineteen":
         lf[i] = 19
+        l[i] = 19
     elif lf[i] == "twenty":
         lf[i] = 20
+        l[i] = 20
     elif lf[i] == "thirty":
         lf[i] = 30
+        l[i] = 30
     elif lf[i] == "forty":
         lf[i] = 40
+        l[i] = 40
     elif lf[i] == "fifty":
         lf[i] = 50
+        l[i] = 50
     elif lf[i] == "sixty":
         lf[i] = 60
+        l[i] = 60
     elif lf[i] == "seventy":
         lf[i] = 70
+        l[i] = 70
     elif lf[i] == "eighty":
         lf[i] = 80
+        l[i] = 80
     elif lf[i] == "ninety":
         lf[i] = 90
+        l[i] = 90
     elif lf[i] == "hundred":
         lf[i] = 100
     elif lf[i] == "thousand":
@@ -75,10 +103,15 @@ for i in range(0,len(lf)):
     elif lf[i] == "crore":
         lf[i] = 10000000
 print(l)
+print(lf)
 for i in range(0,len(lf)):
-    if lf[i] == 100:
-        print(lf[i-1]*100)
+    if lf[i] == 1000000:
+        lt = lf[i-1]*1000000
+ln = [d for d in str(lt)]
 if c == "west":
-    print(lf)
+    for i in range(0,len(ln)):
+        if i%3 == 0:
+            print(ln[i],end="," if (i+3) < len(ln) else "")
+        else:
+            print(ln[i],end="")
 elif c == "ind":
-    print(lf)
