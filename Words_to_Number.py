156b07398842d777ca7757bf559aaae9f025512f
Nityant17 <nityant17@gmail.com>
2025-02-01 12:05:36 +0530

    Completed the code
    made the code shorter for better readability

--- Words_to_Number.py
+++ Words_to_Number.py
@@ -4,105 +4,19 @@ x = a.lower()
 l = x.split(" ")
 lf = l.copy()
 
-for i in range(0,len(lf)):
-    if lf[i] == "zero":
-        lf[i] = 0
-        l[i] = 0
-    elif lf[i] == "one":
-        lf[i] = 1
-        l[i] = 1
-    elif lf[i] == "two":
-        lf[i] = 2
-        l[i] = 2
-    elif lf[i] == "three":
-        lf[i] = 3
-        l[i] = 3
-    elif lf[i] == "four":
-        lf[i] = 4
-        l[i] = 4
-    elif lf[i] == "five":
-        lf[i] = 5
-        l[i] = 5
-    elif lf[i] == "six":
-        lf[i] = 6
-        l[i] = 6
-    elif lf[i] == "seven":
-        lf[i] = 7
-        l[i] = 7
-    elif lf[i] == "eight":
-        lf[i] = 8
-        l[i] = 8
-    elif lf[i] == "nine":
-        lf[i] = 9
-        l[i] = 9
-    elif lf[i] == "ten":
-        lf[i] = 10
-        l[i] = 10
-    elif lf[i] == "eleven":
-        lf[i] = 11
-        l[i] = 11
-    elif lf[i] == "twelve":
-        lf[i] = 12
-        l[i] = 12
-    elif lf[i] == "thirteen":
-        lf[i] = 13
-        l[i] = 13
-    elif lf[i] == "fourteen":
-        lf[i] = 14
-        l[i] = 14
-    elif lf[i] == "fifteen":
-        lf[i] = 15
-        l[i] = 15
-    elif lf[i] == "sixteen":
-        lf[i] = 16
-        l[i] = 16
-    elif lf[i] == "seventeen":
-        lf[i] = 17
-        l[i] = 17
-    elif lf[i] == "eighteen":
-        lf[i] = 18
-        l[i] = 18
-    elif lf[i] == "nineteen":
-        lf[i] = 19
-        l[i] = 19
-    elif lf[i] == "twenty":
-        lf[i] = 20
-        l[i] = 20
-    elif lf[i] == "thirty":
-        lf[i] = 30
-        l[i] = 30
-    elif lf[i] == "forty":
-        lf[i] = 40
-        l[i] = 40
-    elif lf[i] == "fifty":
-        lf[i] = 50
-        l[i] = 50
-    elif lf[i] == "sixty":
-        lf[i] = 60
-        l[i] = 60
-    elif lf[i] == "seventy":
-        lf[i] = 70
-        l[i] = 70
-    elif lf[i] == "eighty":
-        lf[i] = 80
-        l[i] = 80
-    elif lf[i] == "ninety":
-        lf[i] = 90
-        l[i] = 90
-    elif lf[i] == "hundred":
-        lf[i] = 100
-    elif lf[i] == "thousand":
-        lf[i] = 1000
-    elif lf[i] == "million":
-        lf[i] = 1000000
-    elif lf[i] == "billion":
-        lf[i] = 1000000000
-    elif lf[i] == "trillion":
-        lf[i] = 1000000000000
-    elif lf[i] == "lakhs" or lf[i] == "lakh":
-        lf[i] = 100000
-    elif lf[i] == "crore":
-        lf[i] = 10000000
+word_to_num = {
+    "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+    "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, 
+    "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, 
+    "eighty": 80, "ninety": 90, "hundred": 100, "thousand": 1000, "million": 1000000, "billion": 1000000000, 
+    "trillion": 1000000000000, "lakh": 100000, "lakhs": 100000, "crore": 10000000
+}
+
+for i in range(len(lf)):
+    if lf[i] in word_to_num:
+        lf[i] = word_to_num[lf[i]]
+        if lf[i] <= 90: 
+            l[i] = lf[i]
 
 i = 0
 while i < len(lf)-1:
